b63440f1bcacee6359e483a43a46372468c6b303
kolown2007 <kolown@gmail.com>
2024-08-23 22:27:24 +0800

    refactor: Update page component to handle touch events and double-tap detection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,22 @@
 <script lang="ts">
-        import type { PageData } from './$types';
-        
+    import type { PageData } from './$types';
+    import { eventData } from "$lib/ablyclient";
     import type { Scene } from "phaser";
+    import type { scene1 } from "../game/scenes/scene1";
     import PhaserGame, { type TPhaserRef } from "../game/PhaserGame.svelte";
 
     // The sprite can only be moved in the MainMenu Scene
     let canMoveSprite = false;
 
-    //  References to the PhaserGame component (game and scene are exposed)
-    let phaserRef: TPhaserRef = { game: null, scene: null};
-   
+    // References to the PhaserGame component (game and scene are exposed)
+    let phaserRef: TPhaserRef = { game: null, scene: null };
     let lastTap = 0;
-    // Event emitted from the PhaserGame component
-    const currentScene = (scene: Scene) => {
-    canMoveSprite = (scene.scene.key !== "MainMenu");
 
-    }
-
-
+    // Add touchend event listener for double-tap detection
     document.addEventListener('touchend', handleDoubleTap);
+    document.addEventListener('dblclick', reloadWindow);
 
-    function handleDoubleTap(event: any) {
+    function handleDoubleTap(event: TouchEvent) {
         const currentTime = new Date().getTime();
         const tapLength = currentTime - lastTap;
         if (tapLength < 300 && tapLength > 0) {
@@ -29,23 +25,39 @@
         lastTap = currentTime;
     }
 
-    document.addEventListener('dblclick', reloadWindow);
-
     function reloadWindow() {
         window.location.reload();
     }
 
+    const changeScene = () => {
+        const scene = phaserRef.scene as scene1;
 
-    </script>
+        if (scene && typeof scene.changeScene === 'function') {
+            // Call the changeScene method defined in the scene
+            scene.changeScene();
+        }
+    }
 
+    // Event emitted from the PhaserGame component
+    const currentScene = (scene: Scene) => {
+        canMoveSprite = (scene.scene.key !== "MainMenu");
+    }
 
+    // Reactive statement to subscribe to eventData
+    $: {
+        eventData.subscribe(message => {
+            if (message) {
+                console.log("executing...");
+                console.log(message);
+                changeScene();
+            }
+        });
+    }
+</script>
 
 <div id="app">
     <PhaserGame bind:phaserRef={phaserRef} currentActiveScene={currentScene} />
-   
 </div>
-
-
 
 <style>
     #app {
@@ -56,7 +68,4 @@
         justify-content: center;
         align-items: center;
     }
-    
-   
-      
 </style>
