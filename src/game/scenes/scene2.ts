34abc7e7d9815460543da42fd14456ddcc14592c
kolown2007 <kolown@gmail.com>
2024-08-28 17:22:49 +0800

    refactor: Update Preloader to start 'scene2' instead of 'scene5'

--- src/game/scenes/scene2.ts
+++ src/game/scenes/scene2.ts
@@ -22,7 +22,7 @@ export class scene2 extends Scene
         this.camera = this.cameras.main;
         this.camera.setBackgroundColor(0xDA0229);
 
-  
+  this.growRect();
 
         EventBus.emit('current-scene-ready', this);
 
@@ -41,6 +41,34 @@ changeScene() {
     });
 }
 
+
+growRect(){
+
+    const rect = this.add.rectangle(
+        this.cameras.main.centerX, // X position (center of the screen)
+        this.cameras.main.height,  // Y position (bottom of the screen)
+        this.cameras.main.width,                       // Width of the rectangle
+        this.cameras.main.height,  // Height of the rectangle (full screen height)
+        0xb00a28                   // Color of the rectangle
+    );
+
+ 
+    rect.setOrigin(0.5, 1);
+    // Set the initial scale to 0 in the Y direction
+    rect.setScale(1, 0);
+
+    // Add a tween to animate the scaleY property
+    this.tweens.add({
+        targets: rect,
+        scaleY: 1,                // Final scale in the Y direction
+        duration: 30000,           // Duration of the animation in milliseconds
+        onComplete: () => {
+           this.scene.start("scene3")
+        }
+    });
+
+
+}
 
 
 
