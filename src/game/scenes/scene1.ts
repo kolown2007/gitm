befb31109c331bb1d5426313cfa1a165dd4c1ff8
kolown2007 <kolown@gmail.com>
2024-08-29 22:00:25 +0800

    refactor: Update Preloader to start 'scene1' instead of 'scene2'

--- src/game/scenes/scene1.ts
+++ src/game/scenes/scene1.ts
@@ -11,6 +11,8 @@ export class scene1 extends Scene
     camera!: Phaser.Cameras.Scene2D.Camera;
     background!: Phaser.GameObjects.Image;
     gameText!: Phaser.GameObjects.Text;
+    circles: Phaser.GameObjects.Arc[] = [];
+    originalPositions: { x: number, y: number }[] = [];
 
     constructor ()
     {
@@ -37,6 +39,9 @@ export class scene1 extends Scene
             const y = centerY + radius * Math.sin(angle);
     
             const circle = this.add.circle(x, y, circleRadius, 0xffffff);
+            this.circles.push(circle);
+            this.originalPositions.push({ x, y });
+
             this.tweens.add({
                 targets: circle,
                 alpha: 0,
@@ -47,17 +52,43 @@ export class scene1 extends Scene
             });
         }
 
-        //initializeAbly(this, () => this.loveEffects());
+        initializeAbly(this, () => this.loveEffects());
 
         EventBus.emit('current-scene-ready', this);
     }
 
-
     private loveEffects() {
         console.log("still loading");
+        this.flyCirclesRandomly();
+        this.time.delayedCall(2000, () => this.returnCirclesToOriginalPositions());
     }
 
+    private flyCirclesRandomly() {
+        this.circles.forEach(circle => {
+            const randomX = Phaser.Math.Between(0, this.cameras.main.width);
+            const randomY = Phaser.Math.Between(0, this.cameras.main.height);
+            this.tweens.add({
+                targets: circle,
+                x: randomX,
+                y: randomY,
+                duration: 1000,
+                ease: 'Power2'
+            });
+        });
+    }
 
+    private returnCirclesToOriginalPositions() {
+        this.circles.forEach((circle, index) => {
+            const { x, y } = this.originalPositions[index];
+            this.tweens.add({
+                targets: circle,
+                x: x,
+                y: y,
+                duration: 1000,
+                ease: 'Power2'
+            });
+        });
+    }
 
     changeScene() {
         this.cameras.main.fadeOut(1000, 0, 0, 0, (camera: any, progress: number) => {
@@ -66,6 +97,4 @@ export class scene1 extends Scene
             }
         });
     }
-
-
 }
